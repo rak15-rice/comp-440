6102aa7c405a67c41c8f45f6df61600810eda6b1
Riley Kuhlman <rileyk2@icloud.com>
2026-03-31 15:47:29 -0500

    finish off 5

--- tracking/inference.py
+++ tracking/inference.py
@@ -454,7 +454,13 @@ class InferenceModule:
         Return the probability P(noisyDistance | pacmanPosition, ghostPosition).
         """
         "*** YOUR CODE HERE ***"
-        raiseNotDefined()
+        if noisyDistance is None:
+            return 1 if ghostPosition == jailPosition else 0
+        
+        if ghostPosition == jailPosition:
+            return 0
+        
+        return busters.getObservationProbability(noisyDistance, manhattanDistance(pacmanPosition, ghostPosition))
         "*** END YOUR CODE HERE ***"
 
     def setGhostPosition(self, gameState, ghostPosition, index):
